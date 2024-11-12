854d0ec5186177db6556d79e664cef67cf25658f
Gesugao-san <gesugao.san@gmail.com>
2024-11-13 00:57:57 +0300

    My code update

---
+++ default/role.harvester.js
@@ -0,0 +1,45 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.harvester');
+ * mod.thing == 'a thing'; // true
+ */
+
+
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        //let home = creep.room.controller;
+        //let home = Game.spawns['Spawn1'];
+        //creep.moveTo(home);
+        //return;
+        if(creep.store.getFreeCapacity() != 0) {
+            //var sources = creep.room.find(FIND_SOURCES);
+            var sources = Game.spawns['Spawn1'].room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.say('🔄 harvest');
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+    }
+};
+
+module.exports = roleHarvester; //{};
